b96cf7009c1e4b34bd7b275f330d529eed03f6a0
Historia Montague <historia.montague@outlook.com>
2023-04-20 11:07:02 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function isPalindrome(word) {
   // Write your algorithm here
+    for(let n= 0; n < Math.floor(word.length/2); n++) {
+      if(word.charAt(n) === word.charAt(word.length - (n + 1))) {
+        null
+      }
+      else {
+        return false
+      }
+    }
+    return true
 }
 
 /* 
   Add your pseudocode here
+  simple loop that iterates over each character for half the length of the string, rounded down for words with an odd number of letters.
+  checks if the character at the nth position is equal to the character at the -nth position
+  if at any point that test returns false the function returns false as well
 */
 
 /*
@@ -16,10 +28,14 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
-
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("deed"));
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("dead"));
 }
 
 module.exports = isPalindrome;
